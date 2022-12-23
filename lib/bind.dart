bf8e0b7cff7ff514f11c868ca5a3df272b225318
Thomas Dwiatmoko <gabeaventh@gmail.com>
2022-12-23 16:27:21 +0700

    add counter

--- lib/bind.dart
+++ lib/bind.dart
@@ -1,4 +1,5 @@
 import 'package:get/get.dart';
+import 'package:state_research/state_bloc/bloc/counter_cubit.dart';
 import 'package:state_research/state_bloc/bloc/json_bloc_bloc.dart';
 import 'package:state_research/state_bloc/bloc/json_freezed_bloc.dart';
 import 'package:state_research/state_getx/controller.dart';
@@ -9,5 +10,6 @@ class AppBinding implements Bindings {
     Get.lazyPut<JsonBloc>(JsonBloc.new);
     Get.lazyPut<JsonController>(JsonController.new);
     Get.lazyPut<JsonFreezedBloc>(JsonFreezedBloc.new);
+    Get.lazyPut<CounterCubit>(CounterCubit.new);
   }
 }
--- lib/state_bloc/view.dart
+++ lib/state_bloc/view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get/get.dart';
+import 'package:state_research/state_bloc/bloc/counter_cubit.dart';
 import 'package:state_research/state_bloc/bloc/json_freezed_bloc.dart';
 import 'package:state_research/state_bloc/components/empty.dart';
 import 'package:state_research/state_bloc/components/list.dart';
@@ -21,16 +22,35 @@ class BlocView extends StatelessWidget {
       ),
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(24),
-        child: BlocBuilder<JsonFreezedBloc, JsonFreezedState>(
-          bloc: Get.find<JsonFreezedBloc>(),
-          builder: (context, state) {
-            return state.maybeWhen(
-              loading: () => const LoadingWidget(),
-              loaded: (state) => BlocListWidget(data: state?.data),
-              orElse: () => BlocEmptyWidget(onPressed: getJson),
-            );
-          },
+        child: ListView(
+          shrinkWrap: true,
+          children: [
+            BlocBuilder(
+              bloc: Get.find<CounterCubit>(),
+              builder: (context, cState) {
+                return Text('Counter: $cState');
+              },
+            ),
+            const SizedBox(
+              height: 24,
+            ),
+            BlocBuilder<JsonFreezedBloc, JsonFreezedState>(
+              bloc: Get.find<JsonFreezedBloc>(),
+              builder: (context, state) {
+                return state.maybeWhen(
+                  loading: () => const LoadingWidget(),
+                  loaded: (state) => BlocListWidget(data: state?.data),
+                  orElse: () => BlocEmptyWidget(onPressed: getJson),
+                );
+              },
+            ),
+          ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        key: const Key('counterView_increment_floatingActionButton'),
+        child: const Icon(Icons.add),
+        onPressed: () => Get.find<CounterCubit>().increment(),
       ),
     );
   }
